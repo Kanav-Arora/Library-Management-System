38f2a2217db35575171ad0a57e592185a496c744
Kanav-Arora <kanavarora1515@gmail.com>
2021-01-30 20:07:18 +0530

    Backend updatd with add book and delete book functions

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ynusx.mongodb.net/
 myclient = pymongo.MongoClient(uri)
 database = myclient["endsem"]
 
+"----------------------------Register and login----------------------------------------------"
 def register_insert(username, password):
     collection = database["login"]
     dict1 = {"username" : username, "password": password}
@@ -11,4 +12,24 @@ def register_insert(username, password):
 def login_data(username,password):
     x = database.login.count_documents({"$expr":{"$and":[{"$eq":["$username",str(username)]},{"$eq":["$password",str(password)]}]}})
     return x
-   
+"---------------------------------------------------------------------------------------------"
+
+
+"----------------------------------Add Book---------------------------------------------------"
+def add_book(sr, name, author, date, price, category):
+    collection = database["book_list"]
+    dict1 = {"serial number": sr, "name": name, "author": author, "date": date, "price":price, "category": category}
+    x = collection.insert_one(dict1)
+
+# test
+# add_book("B1","Harry Puttar", "J.K Rowling", "2020-01-30",700, "Fiction")
+"---------------------------------------------------------------------------------------------"
+
+
+"---------------------------------Delete Book---------------------------------------------------"
+def delete_book(sr, name):
+    collection = database["book_list"]
+    x = collection.delete_one({"serial number":sr})
+
+# test
+# delete_book("B1", "Harry Puttar")
